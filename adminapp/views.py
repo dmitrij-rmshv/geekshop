cfcb127ae14c0f9d39d26dbb8db78607bf036988
Dmitrij <gdems@operamail.com>
2021-02-11 00:12:24 +0300

    added conditions for controllers

--- adminapp/views.py
+++ adminapp/views.py
@@ -1,18 +1,23 @@
 from django.shortcuts import render, HttpResponseRedirect
 from django.urls import reverse
+from django.contrib.auth.decorators import user_passes_test
 
 from authapp.models import User
 from adminapp.forms import UserAdminRegisterForm, UserAdminProfileForm
 
+
+@user_passes_test(lambda u: u.is_superuser)
 def index(request):
     return render(request, 'adminapp/index.html')
 
 
+@user_passes_test(lambda u: u.is_superuser)
 def admin_users_read(request):
     context = {'users': User.objects.all()}
     return render(request, 'adminapp/admin-users-read.html', context)
 
 
+@user_passes_test(lambda u: u.is_superuser)
 def admin_users_create(request):
     if request.method == 'POST':
         form = UserAdminRegisterForm(data=request.POST, files=request.FILES)
@@ -25,6 +30,7 @@ def admin_users_create(request):
     return render(request, 'adminapp/admin-users-create.html', context)
 
 
+@user_passes_test(lambda u: u.is_superuser)
 def admin_users_update(request, id):
     user = User.objects.get(id=id)
     if request.method == 'POST':
@@ -38,6 +44,7 @@ def admin_users_update(request, id):
     return render(request, 'adminapp/admin-users-update-delete.html', context)
 
 
+@user_passes_test(lambda u: u.is_superuser)
 def admin_users_delete(request, id):
     user = User.objects.get(id=id)
     # user.delete()
