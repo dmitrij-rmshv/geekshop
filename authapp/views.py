ecbca6ed6520e8ee7a1a4b783db272e78be6e79a
Dmitrij <gdems@operamail.com>
2021-03-16 21:36:01 +0300

    media directory opened

--- authapp/views.py
+++ authapp/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, HttpResponseRedirect
+from django.shortcuts import render, HttpResponseRedirect, get_object_or_404
 from django.http import HttpResponse, Http404
 from django.contrib import auth
 from django.urls import reverse
@@ -63,7 +63,8 @@ def profile(request):
 
 
 def verify(request, user_id, hash):
-    user = User.objects.get(pk=user_id)
+    user = get_object_or_404(User, pk=user_id)
+    # user = User.objects.get(pk=user_id)
     if user.activation_key == hash and not user.is_activation_key_expired():
         user.is_active = True
         user.activation_key = None
