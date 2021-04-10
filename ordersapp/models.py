6714e272ca8c6621fb06332bd49f838668f300a8
Dmitrij <gdems@operamail.com>
2021-04-10 19:37:28 +0300

    F-object

--- ordersapp/models.py
+++ ordersapp/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 from django.conf import settings
 from mainapp.models import Product
+from django.db.models import F
 
 
 class Order(models.Model):
@@ -52,7 +53,7 @@ class Order(models.Model):
     # переопределяем метод, удаляющий объект
     def delete(self):
         for item in self.orderitems.select_related():
-            item.product.quantity += item.quantity
+            item.product.quantity = F('quantity') + item.quantity
             item.product.save()
 
         self.is_active = False
